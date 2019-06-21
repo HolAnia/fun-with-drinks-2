c2bacf14d2c743d07dc4838969c185fd6e95094c
HolAnia <anna.holomiej@gmail.com>
2019-06-21 23:16:55 +0200

    Updates

---
+++ precache-manifest.70256a1df9e3e0de7cea4f58d8f0fddd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da7207590a8344afb8f9ae4c7d76aaf",
+    "url": "/fun-with-drinks-2/index.html"
+  },
+  {
+    "revision": "f1ca6aa80e564f53fbba",
+    "url": "/fun-with-drinks-2/static/css/main.fb015949.chunk.css"
+  },
+  {
+    "revision": "f54a6ff2614fa4f02d57",
+    "url": "/fun-with-drinks-2/static/js/2.cb56ed7a.chunk.js"
+  },
+  {
+    "revision": "f1ca6aa80e564f53fbba",
+    "url": "/fun-with-drinks-2/static/js/main.ea95acf0.chunk.js"
+  },
+  {
+    "revision": "a955ec52cf3f84c715c5",
+    "url": "/fun-with-drinks-2/static/js/runtime~main.1b723a9f.js"
+  },
+  {
+    "revision": "14b7f0214a51556e8e6dd332cc467ef4",
+    "url": "/fun-with-drinks-2/static/media/5139493db68077463014ef9382820a92.14b7f021.jpg"
+  },
+  {
+    "revision": "84b6c1b31fa857a76c599be5228afdbc",
+    "url": "/fun-with-drinks-2/static/media/neon-cocktails-sign-by-Vexels.84b6c1b3.png"
+  },
+  {
+    "revision": "d6bc00d28e2e204108ce53a5f5b2f27d",
+    "url": "/fun-with-drinks-2/static/media/shaker.d6bc00d2.svg"
+  }
+]);
